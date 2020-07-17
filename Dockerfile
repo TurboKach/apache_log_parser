f52cf3863441d4520c709331865b283c12335c7b
TurboKach <incognito@turbokach.me>
2020-07-18 01:53:46 +0300

    - added postgres  - added docker-compose.yml

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,12 @@ WORKDIR /usr/src/app
 #  copy requirements to workdir (<src> -> <docker_filesystem>)
 COPY requirements.txt ./
 
+RUN \
+ apk add --no-cache postgresql-libs && \
+ apk add --no-cache --virtual .build-deps gcc musl-dev postgresql-dev && \
+ python3 -m pip install -r requirements.txt --no-cache-dir && \
+ apk --purge del .build-deps
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 #  copy project folder contents to workdir
--- apache_log_parser/settings.py
+++ apache_log_parser/settings.py
@@ -92,8 +92,11 @@ WSGI_APPLICATION = 'apache_log_parser.wsgi.application'
 #  TODO change to postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',  # set in docker-compose.yml
+        'PORT': 5432  # default postgres port
     }
 }
 
